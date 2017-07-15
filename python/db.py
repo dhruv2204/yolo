0a365a010a0c69c4720d290ebcb58e4249c311c6
dhruv <dhruv@gamezop.co>
2017-07-15 20:59:51 +0530

    created db and fetched juniors under some employee(subtree under node)

--- python/db.py
+++ python/db.py
@@ -1,23 +1,40 @@
 import psycopg2
 from config import config
+
+def get_juniors():
+    """ query data from the employees table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("WITH RECURSIVE juniors AS (SELECT e_id, m_id, name FROM "+
+                    "employees WHERE e_id = 2 UNION SELECT e.e_id, e.m_id, e.name "+
+                    "FROM employees e INNER JOIN juniors s ON s.e_id = e.m_id ) SELECT * FROM juniors;")
+        print("The number of employees under e_id 2: ", cur.rowcount)
+        row = cur.fetchone()
  
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+ 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
-        # read connection parameters
         params = config()
  
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
- 
-        # create a cursor
         cur = conn.cursor()
-        
- # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
+        cur.exe
  
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
@@ -34,4 +51,4 @@ def connect():
  
  
 if __name__ == '__main__':
-    connect()
+    get_juniors()
